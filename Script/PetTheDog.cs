27a371899bfaf3f0d673c409988ea3d4d8e878e2
theludovyc <theludovyc@gmail.com>
2022-10-03 21:32:07 +0200

    disable buttons

--- Script/PetTheDog.cs
+++ Script/PetTheDog.cs
@@ -5,14 +5,40 @@ public class PetTheDog : MiniGame
 {
     AnimatedSprite anim = null;
 
+    bool canClick = true;
+
+    TextureButton[] buttons;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         anim = GetNode<AnimatedSprite>("AnimatedSprite");
+
+        buttons = new TextureButton[3]{GetNode<TextureButton>("Dynamite"), GetNode<TextureButton>("Knife"), GetNode<TextureButton>("Hand")};
+    }
+
+    void disableButtons(){
+        foreach(var button in buttons){
+            button.Disabled = true;
+
+            button.Modulate = new Color("#4a4a4a");
+        }
+    }
+
+    void enableButtons(){
+        foreach(var button in buttons){
+            button.Disabled = false;
+
+            button.Modulate = new Color("#ffffff");
+        }
     }
 
     void OnButtonDown(String s){
-        anim.Play(s);
+        if(canClick){
+            anim.Play(s);
+            
+            disableButtons();
+        }
     }
 
     void OnAnimationFinished(){
@@ -21,8 +47,13 @@ public class PetTheDog : MiniGame
                 EmitSignal(nameof(Win));
             break;
 
+            case "Idle":
+            break;
+
             default:
                 anim.Play("Idle");
+
+                enableButtons();
             break;
         }
     }
