7a86e05ac617240b6abb926e86c59315685458f9
Kévin Reilhac <kevin.reilhac.pro@gmail.com>
2022-10-03 16:32:19 +0200

    Add Win & Lose Logic

--- Script/MainGame.cs
+++ Script/MainGame.cs
@@ -20,25 +20,39 @@ public class MainGame : Control
 	private PackedScene GameOver = GD.Load<PackedScene>("res://Scene/GameOver.tscn");
 	private PackedScene WinScene = GD.Load<PackedScene>("res://Scene/Win.tscn");
 	private Stack<MyPopup> popups = new Stack<MyPopup>();
+	private Timer timer = null;
+	private Control winScreen = null;
+	private Control gameOverScreen = null;
+	private bool isGameStoped = false;
 
-	void OnPopupHide() {
+	void OnPopupHide()
+	{
+		if (popups.Count == 0)
+			return;
+
 		var popup = popups.Pop();
 		popup.QueueFree();
 
 		UpdateRamViewer();
-		if (popups.Count > 0) {
+		if (popups.Count > 0)
+		{
 			popups.Peek().SetPauseSubScene(false);
-		}else{
-			GetTree().ChangeSceneTo(WinScene);
+		}
+		else
+		{
+			Win();
 		}
 	}
 
-	Vector2 GetRandomPosition(){
+	Vector2 GetRandomPosition()
+	{
 		return new Vector2(((float)GD.RandRange(10, 990)), (float)GD.RandRange(10, 440));
 	}
 
-	void CreatePopup(String miniGameName){
-		if (popups.Count > 0) {
+	void CreatePopup(String miniGameName)
+	{
+		if (popups.Count > 0)
+		{
 			popups.Peek().SetPauseSubScene(true);
 		}
 
@@ -46,7 +60,7 @@ public class MainGame : Control
 		MiniGame game = miniGames[miniGameName].Instance<MiniGame>();
 
 		currentPopup.AddChildMiniGame(game);
-		AddChild(currentPopup);
+		GetNode("PopUps").AddChild(currentPopup);
 		currentPopup.WindowTitle = miniGameName + ".exe";
 		currentPopup.SetPosition(GetRandomPosition());
 		currentPopup.Connect("popup_hide", this, "OnPopupHide");
@@ -60,35 +74,82 @@ public class MainGame : Control
 		GetNode<Label>("Taskbar/RamText").Text = (string.Format("RAM: {0:00}%", (popups.Count / ((float)maxPopup + 1)) * 100));
 	}
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		foreach(var key in miniGames.Keys.ToList()){
+		GetNodes();
+		StartGame();
+	}
+
+	private void StartGame()
+	{
+		isGameStoped = false;
+		winScreen.Visible = false;
+		gameOverScreen.Visible = false;
+
+		foreach (var key in miniGames.Keys.ToList())
 			miniGames[key] = GD.Load<PackedScene>($"res://Scene/MiniGames/{key}.tscn");
-		}
 
-        for (int i = 0; i < 10; i++)
-        {
-            CreatePopup(GetRandomMiniGameName());
-        }
+		for (int i = 0; i < maxPopup - 1; i++)
+			CreatePopup(GetRandomMiniGameName());
+		timer.Start();
 	}
 
-    private string GetRandomMiniGameName()
-    {
-        return miniGames.Keys.ToArray()[Convert.ToInt32(GD.RandRange(0, miniGames.Count - 1))];
-    }
-
-	void OnTimerTimeout(){
-		if(popups.Count > maxPopup - 1){
-			GetTree().ChangeSceneTo(GameOver);
-		}
-
-		CreatePopup(GetRandomMiniGameName());
+	public override void _Input(InputEvent inputEvent)
+	{
+		if (isGameStoped && (inputEvent is InputEventKey))
+			StartGame();
 	}
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+	private void GetNodes()
+	{
+		winScreen = GetNode<Control>("WinScreen");
+		gameOverScreen = GetNode<Control>("GameOverScreen");
+		timer = GetNode<Timer>("Timer");
+	}
+
+	private string GetRandomMiniGameName()
+	{
+		return miniGames.Keys.ToArray()[Convert.ToInt32(GD.RandRange(0, miniGames.Count - 1))];
+	}
+
+	void OnTimerTimeout()
+	{
+		if (popups.Count > maxPopup - 1)
+			Lose();
+		else
+			CreatePopup(GetRandomMiniGameName());
+	}
+
+	private void StopGame()
+	{
+		while (popups.Count > 0)
+		{
+			MyPopup popup = popups.Pop();
+			popup.QueueFree();
+		}
+
+		timer.Stop();
+	}
+
+	private async void Win()
+	{
+		StopGame();
+		winScreen.SetAsToplevel(true);
+		winScreen.Visible = true;
+		winScreen.GetNode<AnimatedSprite>("WinAnim").Play();
+		
+        await ToSignal(GetTree().CreateTimer(3f), "timeout");
+		isGameStoped = true;
+	}
+
+	private async void Lose()
+	{
+		StopGame();
+
+		gameOverScreen.SetAsToplevel(true);
+		gameOverScreen.Visible = true;
+
+        await ToSignal(GetTree().CreateTimer(3f), "timeout");
+		isGameStoped = true;
+	}
 }
